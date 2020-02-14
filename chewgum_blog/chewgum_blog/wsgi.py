2b6925d95aab523ba3351e2c2421a5afd4e5734b
TomatoChewgum <1304787581@qq.com>
2020-02-14 23:41:50 +0800

    修改 wsgi

--- chewgum_blog/chewgum_blog/wsgi.py
+++ chewgum_blog/chewgum_blog/wsgi.py
@@ -7,10 +7,19 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
+# import os
+#
+# from django.core.wsgi import get_wsgi_application
+#
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chewgum_blog.settings')
+#
+# application = get_wsgi_application()
+
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chewgum_blog.settings')
-
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chewgum_blog.settings')
+profile = os.environ.get('PROJECT_PROFILE','develop')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chewgum_blog.settings.%s' % profile)
 application = get_wsgi_application()
